77471b88566c790ea631ee3dd43276ff04de3b41
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-04 17:00:39 -0600

    Use Foreign Linker API to call strlen
    Replace simple main with an example using the Java FFM (Foreign Function & Memory) API: add imports, create a confined Arena, obtain the native Linker and lookup the "strlen" symbol, build a FunctionDescriptor and downcall handle, allocate a C string, invoke strlen via the handle and print the resulting length. Update main signature to throw Throwable.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,31 @@
 package com.example;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.ValueLayout;
+
+import static java.lang.foreign.Arena.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+        try (Arena arena = ofConfined()) {
+            final var linker = Linker.nativeLinker();
+            final var symbolLookup = linker.defaultLookup();
 
+            final var memorySegment = symbolLookup.find("strlen").orElseThrow();
+
+            final var functionDescriptor = FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS);
+
+            final var methodHandle = linker.downcallHandle(memorySegment, functionDescriptor);
+
+            final var segmentAllocator = arena.allocateFrom("Hello world");
+
+            final var result = (long) methodHandle.invokeExact(segmentAllocator);
+
+            System.out.println(result);
+        }
     }
 
 }
