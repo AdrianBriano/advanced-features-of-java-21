c7e6952805a582c66772b8d32668bcd13406cea6
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-04 16:44:31 -0600

    Use Java 21 and implement KEM decapsulation
    Set Maven compiler source/target to 21 (properties and maven-compiler-plugin) while keeping --enable-preview. Implement Receiver.getKey to perform KEM decapsulation with KEM.getInstance("DHKEM"): create a decapsulator from the private key, decapsulate the encapsulation, and return the encoded secret. Added necessary imports and declared thrown exceptions (NoSuchAlgorithmException, InvalidKeyException, DecapsulateException).

--- src/main/java/com/example/Receiver.java
+++ src/main/java/com/example/Receiver.java
@@ -1,19 +1,21 @@
 package com.example;
 
+import javax.crypto.DecapsulateException;
 import javax.crypto.KEM;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Receiver {
 
-    byte[] getKey(KEM.Encapsulated encapsulated, java.security.PrivateKey privateKey) {
+    byte[] getKey(KEM.Encapsulated encapsulated, java.security.PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeyException, DecapsulateException {
         // Create a new instance of the KEM with the algorithm name "DHKEM"
-
+        final var receiverKem = KEM.getInstance("DHKEM");
         // Create a new Decapsulator using the private key
-
+        final var decapsulator = receiverKem.newDecapsulator(privateKey);
         // Decapsulate the encapsulated key
-
+        final var decapsulated = decapsulator.decapsulate(encapsulated.encapsulation());
         // Return the encoded secret key
-
-        return new byte[0];
+        return decapsulated.getEncoded();
     }
 
 
